8594367544f08eb33edff627d9cbd4aceadceef4
xmhans <xmhans@sky-cloud.net>
2022-06-14 18:44:49 +0800

    feat: init

---
+++ src/main/java/com/github/skycloudtech/apifirewallsdk/ApiFirewallSdkApplication.java
@@ -0,0 +1,13 @@
+package com.github.skycloudtech.apifirewallsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirewallSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirewallSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/skycloudtech/apifirewallsdk/ApiFirewallSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.skycloudtech.apifirewallsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirewallSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
